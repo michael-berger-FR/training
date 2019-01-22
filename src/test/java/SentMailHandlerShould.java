81d0029ceaa1a3a3a6593493c0b872c4c0689594
Michael Berger <michael.berger@orange.com>
2019-01-22 11:34:55 +0100

    SentMailHandler

--- src/test/java/SentMailHandlerShould.java
+++ src/test/java/SentMailHandlerShould.java
@@ -7,8 +7,10 @@ import java.util.Map;
 public class SentMailHandlerShould {
     @Test
     public void createAndStoreMailSentProjectionWhenReceiveMailSent() {
-        MailSentHandler mailSentHandler = new MailSentHandler(new HashMap<>());
+        MailSentHandler mailSentHandler = new MailSentHandler(new MailSentRepository());
+
         mailSentHandler.handle(new MailSentEvent(1, "hello"));
+
         Assertions.assertThat(mailSentHandler.getSentMailProjection(1)).isNotNull();
         Assertions.assertThat(mailSentHandler.getSentMailProjection(1).getContent()).isEqualTo("hello");
     }
@@ -16,8 +18,8 @@ public class SentMailHandlerShould {
     @Test
     public void deleteMailSentProjectionWhenDeletedMailEventWithSentMail() {
         // Given
-        Map<Integer, SentMailProjection> repository = new HashMap<>();
-        repository.put(1, new SentMailProjection(1, "hello"));
+        MailSentRepository repository = new MailSentRepository();
+        repository.put(1, new MailSentProjection(1, "hello"));
         MailSentHandler mailSentHandler = new MailSentHandler(repository);
 
         // When
@@ -28,17 +30,19 @@ public class SentMailHandlerShould {
     }
 
     private class MailSentHandler {
-        Map<Integer, SentMailProjection> repository = new HashMap<>();
 
-        public MailSentHandler(Map<Integer, SentMailProjection> repository) {
+
+        private final MailSentRepository repository;
+
+        public MailSentHandler(MailSentRepository repository) {
             this.repository = repository;
         }
 
         public void handle(MailSentEvent hello) {
-            repository.put(hello.id, new SentMailProjection(hello.id, hello.content));
+            repository.put(hello.id, new MailSentProjection(hello.id, hello.content));
         }
 
-        public SentMailProjection getSentMailProjection(int i) {
+        public MailSentProjection getSentMailProjection(int i) {
             return repository.get(i);
         }
 
@@ -58,11 +62,11 @@ public class SentMailHandlerShould {
         }
     }
 
-    private class SentMailProjection {
+    private class MailSentProjection {
         private final String content;
         private final Integer id;
 
-        public SentMailProjection(Integer id, String content) {
+        public MailSentProjection(Integer id, String content) {
             this.id = id;
             this.content = content;
         }
@@ -78,6 +82,22 @@ public class SentMailHandlerShould {
 
         public MailDeleted(int id) {
             this.id = id;
+        }
+    }
+
+    private class MailSentRepository {
+        Map<Integer, MailSentProjection> repository = new HashMap<>();
+
+        public void put(Integer id, MailSentProjection sentMailProjection) {
+            repository.put(id, sentMailProjection);
+        }
+
+        public MailSentProjection get(int i) {
+            return repository.get(i);
+        }
+
+        public void remove(int id) {
+            repository.remove(id);
         }
     }
 }
